0703885086d0eba0caed4ccc478f75a58c493e13
Ali Ahmad <143416088+aliahmad884@users.noreply.github.com>
2024-08-27 17:56:56 +0500

    Update unzip.php

--- unzip.php
+++ unzip.php
@@ -1,13 +1,13 @@
 <?php
-// $zip = new ZipArchive;
-// $res = $zip->open('next.zip');
-// if ($res === TRUE) {
-//   $zip->extractTo('/');
-//   $zip->close();
-//   echo 'Unzipped successfully!<br>';
+$zip = new ZipArchive;
+$res = $zip->open('next.zip');
+if ($res === TRUE) {
+  $zip->extractTo('/');
+  $zip->close();
+  echo 'Unzipped successfully!<br>';
   
   // List the contents of the directory
-  $dir = '/'; // Root directory
+  $dir = '/home/wecarhir/'; // Root directory
   if (is_dir($dir)) {
     if ($dh = opendir($dir)) {
       echo "<h3>Contents of $dir:</h3>";
