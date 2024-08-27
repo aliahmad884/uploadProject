c714707c961c6a84a64033e7ccaa29a9c9664471
Ali Ahmad <143416088+aliahmad884@users.noreply.github.com>
2024-08-27 18:06:14 +0500

    Update unzip.php

--- unzip.php
+++ unzip.php
@@ -7,23 +7,23 @@ if ($res === TRUE) {
   echo 'Unzipped successfully!<br>';
   
   // List the contents of the directory
-  $dir = '/home/wecarhir/test06.aldortech.com/'; // Root directory
-  if (is_dir($dir)) {
-    if ($dh = opendir($dir)) {
-      echo "<h3>Contents of $dir:</h3>";
-      echo "<ul>";
-      while (($file = readdir($dh)) !== false) {
-        echo "<li>" . htmlspecialchars($file) . "</li>";
-      }
-      echo "</ul>";
-      closedir($dh);
-    } else {
-      echo 'Failed to open directory.';
-    }
-  } else {
-    echo 'Not a valid directory.';
-  }
-} else {
-  echo 'Failed to unzip.';
-}
+//   $dir = '/home/wecarhir/test06.aldortech.com/'; // Root directory
+//   if (is_dir($dir)) {
+//     if ($dh = opendir($dir)) {
+//       echo "<h3>Contents of $dir:</h3>";
+//       echo "<ul>";
+//       while (($file = readdir($dh)) !== false) {
+//         echo "<li>" . htmlspecialchars($file) . "</li>";
+//       }
+//       echo "</ul>";
+//       closedir($dh);
+//     } else {
+//       echo 'Failed to open directory.';
+//     }
+//   } else {
+//     echo 'Not a valid directory.';
+//   }
+// } else {
+//   echo 'Failed to unzip.';
+// }
 ?>
